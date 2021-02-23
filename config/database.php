e0e3b0779cef46d6964cb33a07931abf58a30fd9
ibrahimmina <71660135+ibrahimmina@users.noreply.github.com>
2021-02-23 16:33:53 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-221-49-44.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd99ljat8laqmlf'),
+            'username' => env('DB_USERNAME', 'ojwzimunzlurdc'),
+            'password' => env('DB_PASSWORD', 'f5904155db30ae695b70b6390b765f7a6b08271e3a79122079c1a18ec3126d2b'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
